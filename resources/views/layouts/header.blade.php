dd01554d0e85a86d113a5866b80031319b3d46b4
Elliot Derhay <JSn1nj4@users.noreply.github.com>
2021-07-22 21:44:24 -0400

    Make theme button show on all screen sizes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,19 +17,6 @@
 				</label>
 			</div>
 
-			<input type="checkbox" id="dark-mode-toggle" name="dark-mode-toggle" class="hidden absolute top-0 left-0 -z-50">
-
-			<div class="block lg:hidden mr-3 border-l-2 border-solid border-sea-green-800 pl-3" id="dark-mode-toggle-button">
-				<label for="dark-mode-toggle" class="flex items-center text-sea-green-500 hover:text-gray-500 opacity-70 transition-colors duration-300">
-					<svg id="light-mode" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-					</svg>
-					<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-					</svg>
-				</label>
-			</div>
-
 			<input type="checkbox" id="menu-toggle" name="menu-toggle" class="hidden absolute top-0 left-0 -z-50">
 
 			<div class="w-full block absolute lg:relative flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl mobile-menu">
@@ -46,6 +33,19 @@
 					@endforeach
 
 				</div>
+			</div>
+
+			<input type="checkbox" id="dark-mode-toggle" name="dark-mode-toggle" class="hidden absolute top-0 left-0 -z-50">
+
+			<div class="block mr-3 border-l-2 border-solid border-sea-green-800 pl-3" id="dark-mode-toggle-button">
+				<label for="dark-mode-toggle" class="flex items-center text-sea-green-500 hover:text-gray-500 opacity-70 transition-colors duration-300">
+					<svg id="light-mode" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+					</svg>
+					<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+					</svg>
+				</label>
 			</div>
 
 		</nav>
