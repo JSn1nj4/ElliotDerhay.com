a3267fee522efaf01b9692587d406301d4d25502
Elliot Derhay <elliot@elliotderhay.com>
2022-02-26 09:55:40 -0500

    Add command and related event

---
+++ app/Events/GithubUsersUpdatedEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+
+class GithubUsersUpdatedEvent
+{
+    use Dispatchable;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
