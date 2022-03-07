e4ffc5d25f4dc32c7e18539181db7dd5f7d596e8
Elliot Derhay <elliot@elliotderhay.com>
2022-03-06 23:03:27 -0500

    Finish first validator factory test

--- tests/Unit/App/Factories/TwitterUserValidatorFactoryTest.php
+++ tests/Unit/App/Factories/TwitterUserValidatorFactoryTest.php
@@ -43,6 +43,74 @@ it('builds the correct rule set', function (): void {
 		->toHaveKeys(['id', 'name']);
 });
 
-// builds the correct messages set
+/**
+ * @todo: add overrides if custom messages are added to the factory
+ * classes in the future
+ */
+it('builds the correct messages set', function (): void {
+	$message_sets = new class {
+		public array $empty = [];
+		public array $extra = [
+			'required' => 'The field called :attribute is a required field.',
+		];
+//		public array $overrides = [
+//			'' => '',
+//			'' => '',
+//		];
+	};
 
-// builds the correct custom attributes set
+	$defaults = TwitterUserValidatorFactory::messages([]);
+
+	$with_empty = TwitterUserValidatorFactory::messages($message_sets->empty);
+	$with_extra = TwitterUserValidatorFactory::messages($message_sets->extra);
+//	$with_overrides = TwitterUserValidatorFactory::messages($message_sets->overrides);
+
+	expect(array_diff_assoc($with_empty, $defaults))
+		->toBeArray()
+		->toHaveCount(0)
+		->and(array_diff_assoc($with_extra, $defaults))
+		->toBeArray()
+		->toHaveCount(1)
+		->toHaveKey('required', 'The field called :attribute is a required field.');
+//		->toHaveKey('unknown_property', 'null')
+//		->and(array_diff_assoc($with_overrides, $defaults))
+//		->toBeArray()
+//		->toHaveCount(2)
+//		->toHaveKeys(['id', 'name']);
+});
+
+/**
+ * @todo: add overrides if custom attributes are added to the factory
+ * classes in the future
+ */
+it('builds the correct custom attributes set', function (): void {
+	$attribute_sets = new class {
+		public array $empty = [];
+		public array $extra = [
+			'screen_name' => 'screen name',
+		];
+//		public array $overrides = [
+//			'' => '',
+//			'' => '',
+//		];
+	};
+
+	$defaults = TwitterUserValidatorFactory::messages([]);
+
+	$with_empty = TwitterUserValidatorFactory::messages($attribute_sets->empty);
+	$with_extra = TwitterUserValidatorFactory::messages($attribute_sets->extra);
+//	$with_overrides = TwitterUserValidatorFactory::messages($message_sets->overrides);
+
+	expect(array_diff_assoc($with_empty, $defaults))
+		->toBeArray()
+		->toHaveCount(0)
+		->and(array_diff_assoc($with_extra, $defaults))
+		->toBeArray()
+		->toHaveCount(1)
+		->toHaveKey('screen_name', 'screen name');
+//		->toHaveKey('unknown_property', 'null')
+//		->and(array_diff_assoc($with_overrides, $defaults))
+//		->toBeArray()
+//		->toHaveCount(2)
+//		->toHaveKeys(['id', 'name']);
+});
