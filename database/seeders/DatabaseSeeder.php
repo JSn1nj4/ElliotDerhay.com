4493cb0ed35928e8c7bea78727e5441f6f8dfe31
Elliot Derhay <elliot@elliotderhay.com>
2022-04-30 13:27:00 -0400

    Wire up blog seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		 $this->call([]);
+		 $this->call([
+			 CategorySeeder::class,
+			 PostSeeder::class,
+			 TagSeeder::class,
+		 ]);
 	}
 }
