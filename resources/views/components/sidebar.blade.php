863bf7f255840dd544300ab5b4a9890ea50ed9ba
Elliot Derhay <elliot@elliotderhay.com>
2022-05-09 13:19:57 -0400

    Tweak blog layout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,3 @@
-<x-column class="w-full md:w-1/3">
+<x-column class="w-full lg:w-1/3">
   {{ $slot }}
 </x-column>
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.page', ['bodyClasses' => $bodyClasses ?? ''])
 
 @section('content')
-	<x-row>
-		<x-column id="blog" class="flex-1 pr-8 last:pr-0">
+	<x-row flex-class="lg:flex">
+		<x-column id="blog" class="flex-1 pr-8 last:pr-0 pb-12 lg:pb-0">
 			@yield('blog')
 		</x-column>
 
