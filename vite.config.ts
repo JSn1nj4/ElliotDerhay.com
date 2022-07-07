7e102e40ee275948295f41ed6dc7b697b303069b
Elliot Derhay <elliot@elliotderhay.com>
2022-07-06 20:39:53 -0400

    Tweak vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,30 @@ import {defineConfig, HmrOptions, loadEnv, ServerOptions, UserConfig} from 'vite
 import laravel from 'laravel-vite-plugin'
 import vuePlugin from "@vitejs/plugin-vue"
 
-const mode = ({ mode }) => mode
+export default defineConfig(({ mode }) => {
+	// setup
+	Object.assign(process.env, loadEnv(mode, process.cwd()))
+	const {VITE_SERVER_HOST, VITE_SERVER_HTTPS} = process.env
 
-Object.assign(process.env, loadEnv(mode, process.cwd()))
+	// build hmr config
+	const hmr: HmrOptions = {}
 
-const hmr: HmrOptions = {}
+	// build vite server config
+	const server: ServerOptions = {}
+	if(Object.keys(hmr).length > 0) server.hmr = hmr
+	if(VITE_SERVER_HOST) server.host = VITE_SERVER_HOST !== 'true' ? VITE_SERVER_HOST : true
+	if(VITE_SERVER_HTTPS) server.https = VITE_SERVER_HTTPS === 'true'
 
-const server: ServerOptions = {}
+	// build viteConfig object
+	const viteConfig: UserConfig = {}
+	if(Object.keys(server).length > 0) viteConfig.server = server
+	viteConfig.plugins = [
+		laravel([
+			'resources/css/app.css',
+			'resources/js/app.js',
+		]),
+		vuePlugin(),
+	]
 
-if(Object.keys(hmr).length > 0) server.hmr = hmr
-
-if(!!process.env.VITE_SERVER_HOST) server.host = process.env.VITE_SERVER_HOST
-server.https = !!process.env.VITE_SERVER_HTTPS && process.env.VITE_SERVER_HTTPS === 'true'
-
-const viteConfig: UserConfig = {}
-
-if(Object.keys(server).length > 0) viteConfig.server = server
-viteConfig.plugins = [
-	laravel([
-		'resources/css/app.css',
-		'resources/js/app.js',
-	]),
-	vuePlugin(),
-]
-
-export default defineConfig(viteConfig)
+	return viteConfig
+})
