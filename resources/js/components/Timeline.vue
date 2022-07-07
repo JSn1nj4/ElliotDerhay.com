7254c705a263b194d3f9671f67abfc17a94f29d2
Elliot Derhay <elliot@elliotderhay.com>
2022-07-06 22:28:40 -0400

    More TS and composition API refactoring

--- resources/js/components/Timeline.vue
+++ resources/js/components/Timeline.vue
@@ -2,7 +2,7 @@
 	<div class="timeline relative mb-4">
 
 		<div v-if="showLine" :class="`${commonClasses} top-0 border-solid h-full`">&nbsp;</div>
-		<slot></slot>
+		<slot />
 		<div v-if="showDottedLine" :class="`${commonClasses} bottom-0 border-dashed h-4 -mb-4`">&nbsp;</div>
 
 	</div>
@@ -10,19 +10,14 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const props = defineProps({
-	showLine: {
-		type: Boolean,
-		default: true
-	},
-	showDottedLine: {
-		type: Boolean,
-		default: false
-	},
-	linePositionClass: {
-		type: String,
-		default: 'w-10'
-	}
+const props = withDefaults(defineProps<{
+	showLine?: boolean,
+	showDottedLine?: boolean,
+	linePositionClass?: string,
+}>(), {
+	showLine: true,
+	showDottedLine: false,
+	linePositionClass: 'w-10',
 })
 
 const commonClasses = computed(() => `absolute ${props.linePositionClass} border-r border-gray-600`)
