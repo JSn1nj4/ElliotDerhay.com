f45903fd1e69ac23f01765186bd723edf3fce702
Elliot Derhay <elliot@elliotderhay.com>
2022-09-01 22:11:10 -0400

    Add blog config to control enable/disable blog features

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -21,19 +21,27 @@
 @endsection
 
 @section('sidebar')
-	<x-widget.wrapper title="Categories">
-		<x-widget.blog.categories display="list"/>
-	</x-widget.wrapper>
+	@if(config('blog.feature.categories_widget'))
+		<x-widget.wrapper title="Categories">
+			<x-widget.blog.categories display="list"/>
+		</x-widget.wrapper>
+	@endif
 
-	<x-widget.wrapper title="Tags">
-		<x-widget.blog.tags sort-by="count" limit="10"/>
-	</x-widget.wrapper>
+	@if(config('blog.feature.tags_widget'))
+		<x-widget.wrapper title="Tags">
+			<x-widget.blog.tags sort-by="count" limit="10"/>
+		</x-widget.wrapper>
+	@endif
 
-	<x-widget.wrapper title="Latest Tweet">
-		<x-twitter.timeline count="1"/>
-	</x-widget.wrapper>
+	@if(config('blog.feature.latest_tweets_widget'))
+		<x-widget.wrapper title="Latest Tweet">
+			<x-twitter.timeline count="1"/>
+		</x-widget.wrapper>
+	@endif
 
-	<x-widget.wrapper title="GitHub Activity">
-		<x-github.events-feed count="5"/>
-	</x-widget.wrapper>
+	@if(config('blog.feature.github_activity_widget'))
+		<x-widget.wrapper title="GitHub Activity">
+			<x-github.events-feed count="5"/>
+		</x-widget.wrapper>
+	@endif
 @endsection
