eb12dd93248bce1d4313ab437d61d226bab8a699
Elliot Derhay <elliot@elliotderhay.com>
2022-09-03 22:25:14 -0400

    Start work on single post appearance

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -2,8 +2,9 @@
 @extends('layouts.blog')
 
 @section('blog')
-	<h1>{{ $post->title }}</h1>
-	<p>{{ $post->body }}</p>
+	<img src="{{ $post->cover_image }}" class="block rounded-lg" alt="">
+	<h1 class="content-title text-4xl pt-3 mt-2">{{ $post->title }}</h1>
+	<p class="mb-4 text-xl">{{ $post->body }}</p>
 @endsection
 
 @section('sidebar')
