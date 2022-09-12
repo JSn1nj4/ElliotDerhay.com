414a69512dbf73c4501abae34f8d54a951aea6c3
Elliot Derhay <elliot@elliotderhay.com>
2022-09-11 23:50:06 -0400

    Fix Vite + HTTPS issue
    Finally resolved this dumb issue.
    
    - Update all Yarn packages, especially ones related to Vite
    - Add settings to configure custom Vite port and HMR host via .env
    - Tell Vite where cert files are if using HTTPS
    - Ensure appserver container built with a handful of high port numbers
    - Make Vite default to lowest port number in Landofile
    - Install Node into appserver
    - Add tooling for running `yarn dev` as root
    
    Why laravel-vite needed updating: https://github.com/laravel/vite-plugin/pull/42

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
 import {defineConfig, HmrOptions, loadEnv, ServerOptions, UserConfig} from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vuePlugin from "@vitejs/plugin-vue"
+import * as fs from "fs"
 
 export default defineConfig(({ mode }) => {
 	// setup
 	Object.assign(process.env, loadEnv(mode, process.cwd()))
 	const {
 		VITE_SERVER_HOST,
-		VITE_SERVER_HTTPS
+		VITE_SERVER_HMR_HOST,
+		VITE_SERVER_HTTPS,
+		VITE_SERVER_PORT
 	} = process.env
 
 	// build hmr config
 	const hmr: HmrOptions = {}
+	if(VITE_SERVER_HMR_HOST) hmr.host = VITE_SERVER_HMR_HOST
 
 	// build vite server config
 	const server: ServerOptions = {}
 	if(Object.keys(hmr).length > 0) server.hmr = hmr
 	if(VITE_SERVER_HOST) server.host = VITE_SERVER_HOST !== 'true' ? VITE_SERVER_HOST : true
-	if(VITE_SERVER_HTTPS) server.https = VITE_SERVER_HTTPS === 'true'
+	if(VITE_SERVER_HTTPS) server.https = VITE_SERVER_HTTPS === 'true' ? {
+		cert: fs.readFileSync('/certs/cert.crt'),
+		key: fs.readFileSync('/certs/cert.key'),
+    } :  false
+	server.port = VITE_SERVER_PORT ? parseInt(VITE_SERVER_PORT) : 24690
 
 	// build viteConfig object
 	const viteConfig: UserConfig = {}
