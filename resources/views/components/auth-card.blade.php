6453359526bdfff4770d8b8e5696a5f9c22f0696
Elliot Derhay <elliot@elliotderhay.com>
2022-10-20 17:56:56 -0400

    Tweak login form top margin

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -6,7 +6,7 @@
 			</div>
 		@endisset
 
-    <div class="w-full sm:max-w-sm mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md dark:shadow-gray-700 overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-sm mt-6 first:mt-0 px-6 py-4 bg-white dark:bg-gray-800 shadow-md dark:shadow-gray-700 overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
