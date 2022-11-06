9f8da71a76d9ab90f6a2576e43027033a9bccacb
Elliot Derhay <elliot@elliotderhay.com>
2022-11-05 23:06:32 -0400

    Fix per_page filter

--- app/Definitions/Traits/PerPageHelpers.php
+++ app/Definitions/Traits/PerPageHelpers.php
@@ -11,18 +11,19 @@ trait PerPageHelpers
 {
 	public static function isIntegerLike(mixed $value): bool
 	{
-		return ((string)intval($value)) === "$value";
+		return is_numeric($value) && ((string)intval($value)) === "$value";
 	}
 
 	public static function filter(mixed $value): int
 	{
+		if(!self::isIntegerLike($value)) return self::DEFAULT->value;
+
 		$int = intval($value);
 
 		return match(true) {
 			$int < self::MIN->value => self::MIN->value,
 			$int > self::MAX->value => self::MAX->value,
-			self::isIntegerLike($value) => $int,
-			default => self::DEFAULT->value,
+			default => $int,
 		};
 	}
 }
