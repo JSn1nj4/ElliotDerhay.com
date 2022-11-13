60cf9a79ef411b84cb62916bef97a93e1de48fa1
Elliot Derhay <elliot@elliotderhay.com>
2022-11-13 16:46:36 -0500

    Generate missing IDE helper model docs

--- app/Models/Login.php
+++ app/Models/Login.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Login
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Login newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Login newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Login query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Login whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Login whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Login whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Login whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Login extends Model
 {
     use HasFactory;
