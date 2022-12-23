151da23332b70b7743b56d4d98c8893a63eeb113
Elliot Derhay <elliot@elliotderhay.com>
2022-12-23 17:18:09 -0500

    Tweak password reset flow defaults
    - views now use the site layout and theme
    - behavior is slightly different for prod
    - translation file is updated

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,11 +1,8 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.page')
 
+@section('content')
+
+    <x-auth-card>
         <form method="POST" action="{{ route('password.update') }}">
             @csrf
 
@@ -48,4 +45,5 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+
+@endsection
