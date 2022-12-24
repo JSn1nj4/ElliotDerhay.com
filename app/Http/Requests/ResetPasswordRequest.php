c1bf1d1d995030608687afd25caabca6221554b5
Elliot Derhay <elliot@elliotderhay.com>
2022-12-23 22:44:54 -0500

    Add property hinting to ResetPasswordRequest

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -5,6 +5,11 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
 
+/**
+ * @property-read string $email
+ * @property-read string $password
+ * @property-read string $token
+ */
 class ResetPasswordRequest extends FormRequest
 {
     public function rules(): array
