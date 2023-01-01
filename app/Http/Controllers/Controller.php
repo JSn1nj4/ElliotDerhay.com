721b92ccb34a7f82574cfcbd12714ff5b5143419
Elliot Derhay <elliot@elliotderhay.com>
2022-12-31 21:02:53 -0500

    Add conditional `dispatchIf` method
    Borrowed from Zach

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+	public function dispatchIf(bool $condition, ShouldQueue $job)
+	{
+		return $condition ? $this->dispatch($job) : false;
+	}
 }
