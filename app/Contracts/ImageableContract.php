b1be842dae735ee4ab2fcbd2362ba22546d87d34
Elliot Derhay <elliot@elliotderhay.com>
2023-01-03 22:54:11 -0500

    `image()` returns the latest image for a model now
    With a morph many-to-many, there's no one-of-many available. So using an attribute will help work around this limitation.

--- app/Contracts/ImageableContract.php
+++ app/Contracts/ImageableContract.php
@@ -2,11 +2,12 @@
 
 namespace App\Contracts;
 
-use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 interface ImageableContract
 {
-	public function image(): MorphOne;
+	public function image(): Attribute;
+
 	public function images(): MorphToMany;
 }
--- app/Traits/Imageable.php
+++ app/Traits/Imageable.php
@@ -3,14 +3,14 @@
 namespace App\Traits;
 
 use App\Models\Image;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 trait Imageable
 {
-	public function image(): MorphOne
+	public function image(): Attribute
 	{
-		return $this->morphOne(Image::class, 'imageable')->latestOfMany();
+		return Attribute::get(fn () => $this->images()->first());
 	}
 
 	public function images(): MorphToMany
