300b41303790b16339160430d8648747f7d36d1b
Elliot Derhay <elliot@elliotderhay.com>
2023-05-20 11:11:57 -0400

    Caption will just overlay
    This is the simplest path forward and bypasses needing extra calculations

--- resources/js/Lightbox.vue
+++ resources/js/Lightbox.vue
@@ -1,17 +1,26 @@
 <template>
 	<section class="lightbox overlay fixed bg-black/50 z-50 w-screen min-h-screen top-0">
 		<button class="absolute top-2 right-4 text-4xl z-50">&times;</button>
-		<div :style="{ gap }" class="flex flex-col justify-center items-center min-h-screen max-h-screen px-16 py-8 gap-4">
-			<div class="relative w-full block overflow-hidden">
-				<img :style="{ maxHeight }" class="" :class="props.classes" :src="props.src" :alt="props.alt" :title="props.title" :srcset="props.srcset">
+		<div class="relative min-h-screen max-h-screen px-16 py-8">
+			<div class="relative block overflow-hidden">
+				<img
+					class="relative mx-auto object-contain"
+					:src="props.src"
+					:alt="props.alt"
+					:title="props.title"
+					:srcset="props.srcset">
+				<figcaption
+					v-if="props.caption"
+					class="caption absolute bottom-0 left-0 block w-full p-4 text-center bg-black/30">
+					{{ props.caption }}
+				</figcaption>
 			</div>
-			<figcaption ref="caption" class="flex-none">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repellat.</figcaption>
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
-import {computed, ref, unref} from "vue";
+import {computed, onActivated, onMounted, Ref, ref, unref} from "vue";
 // import {Image} from "./components/Image.vue";
 // init close button with close event
 // define shape of expected lightbox data
@@ -28,23 +37,19 @@ import {computed, ref, unref} from "vue";
 // hide lightbox
 // clear lightbox state
 
-const captionHeight = ref<number>(100);
-
-const gapVal = ref<number>(1);
-
-const gap = computed<string>(() => {
-	return `${unref(gapVal)}rem`
-})
-
-const maxHeight = computed<string>(() => {
-	return `calc(100vh - ${unref(gapVal)}rem - ${unref(captionHeight)}px)`
-})
-
 const props = {
 	alt: 'fake alt',
-	classes: 'relative mx-auto object-contain',
 	src: 'https://fakeimg.pl/768x576/282828/eae0d0/?retina=1',
 	srcset: null,
 	title: 'fake title',
+	caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repellat.',
 }
 </script>
+
+<style lang="postcss">
+.lightbox {
+	img	{
+		max-height: calc(100vh - theme(spacing.8) * 2);
+	}
+}
+</style>
