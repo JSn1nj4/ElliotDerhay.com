4162e5ceaabad658295380a3535c578e2fbea507
Elliot Derhay <elliot@elliotderhay.com>
2023-05-20 23:31:25 -0400

    Create open, close, and update functions

--- resources/js/Lightbox.vue
+++ resources/js/Lightbox.vue
@@ -1,13 +1,24 @@
 <template>
-	<section class="lightbox overlay fixed bg-black/50 z-50 w-screen min-h-screen top-0">
-		<button class="absolute top-2 right-4 text-4xl z-50">&times;</button>
+	<section
+		class="lightbox overlay fixed transition-opacity bg-black/50 w-screen min-h-screen top-0"
+		:class="{
+			'z-50': front,
+			'-z-50': !front,
+			'opacity-0': !visible,
+		}"
+		:style="{
+			transitionDuration: speed,
+			transitionTimingFunction: timing,
+		}"
+	>
+		<button @click="triggerClose" class="absolute top-2 right-4 text-4xl z-50">&times;</button>
 		<div class="relative min-h-screen max-h-screen px-16 py-8">
 			<CaptionedImage
-				:src="imageData.src"
-				:srcset="imageData.srcset"
-				:alt="imageData.alt"
-				:title="imageData.title"
-				:caption="imageData.caption"
+				:src="image.src"
+				:srcset="image.srcset"
+				:alt="image.alt"
+				:title="image.title"
+				:caption="image.caption"
 			/>
 		</div>
 	</section>
@@ -16,29 +27,70 @@
 <script setup lang="ts">
 import CaptionedImage from "./components/CaptionedImage.vue";
 import {ImageWithCaption} from "./components/ImageTypes";
+import {ref, Ref} from "vue";
 
-// init close button with close event
-// define shape of expected lightbox data
-	// src
-	// alternate sources
-	// alt
-	// title
-	// caption?
+type duration =
+	| 0
+	| 75
+	| 100
+	| 150
+	| 200
+	| 300
+	| 500
+	| 700
+	| 1000
 
-// receive update event
-// show lightbox if hidden
+type timing =
+	| 'linear'
 
-// receive close event
-// hide lightbox
-// clear lightbox state
+const front: Ref<boolean> = ref(true) // TODO: default false
+const visible: Ref<boolean> = ref(true) // TODO: default false
 
-const imageData: ImageWithCaption = {
+const image: Ref<ImageWithCaption> = ref({
 	alt: 'fake alt',
 	src: 'https://fakeimg.pl/768x576/282828/eae0d0/?retina=1',
 	srcset: null,
 	title: 'fake title',
 	caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repellat.',
+})
+
+function triggerClose(e) {
+	visible.value = false
+
+	setTimeout(() => {
+		front.value = false
+
+		triggerUpdate({ src: '' })
+	}, props.speed)
 }
+
+function triggerUpdate(data: ImageWithCaption) {
+	image.value = data
+}
+
+function triggerOpen(e) {
+	// TODO: set image if has ImageWithCaption
+	// triggerUpdate(e.imageData)
+
+	if (front.value === false) front.value = true
+	if (visible.value === true) return
+
+	setTimeout(() => {
+		visible.value = true
+	}, props.speed)
+}
+
+const props = withDefaults(defineProps<{
+	speed?: duration,
+	timing?: timing,
+	transition?: boolean,
+}>(), {
+	speed: 300,
+	timing: "linear",
+	transition: true,
+})
+
+// TODO: receive update event
 </script>
 
 <style lang="postcss">
