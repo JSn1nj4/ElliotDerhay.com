540ecdb8e5cbc9da2ac778ec123248d1ececc0a4
Elliot Derhay <elliot@elliotderhay.com>
2023-05-20 22:53:08 -0400

    Create captioned image component

--- resources/js/Lightbox.vue
+++ resources/js/Lightbox.vue
@@ -2,26 +2,21 @@
 	<section class="lightbox overlay fixed bg-black/50 z-50 w-screen min-h-screen top-0">
 		<button class="absolute top-2 right-4 text-4xl z-50">&times;</button>
 		<div class="relative min-h-screen max-h-screen px-16 py-8">
-			<div class="relative block overflow-hidden">
-				<img
-					class="relative mx-auto object-contain"
-					:src="props.src"
-					:alt="props.alt"
-					:title="props.title"
-					:srcset="props.srcset">
-				<figcaption
-					v-if="props.caption"
-					class="caption absolute bottom-0 left-0 block w-full p-4 text-center bg-black/30">
-					{{ props.caption }}
-				</figcaption>
-			</div>
+			<CaptionedImage
+				:src="imageData.src"
+				:srcset="imageData.srcset"
+				:alt="imageData.alt"
+				:title="imageData.title"
+				:caption="imageData.caption"
+			/>
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
-import {computed, onActivated, onMounted, Ref, ref, unref} from "vue";
-// import {Image} from "./components/Image.vue";
+import CaptionedImage from "./components/CaptionedImage.vue";
+import {ImageWithCaption} from "./components/ImageTypes";
+
 // init close button with close event
 // define shape of expected lightbox data
 	// src
@@ -37,7 +32,7 @@ import {computed, onActivated, onMounted, Ref, ref, unref} from "vue";
 // hide lightbox
 // clear lightbox state
 
-const props = {
+const imageData: ImageWithCaption = {
 	alt: 'fake alt',
 	src: 'https://fakeimg.pl/768x576/282828/eae0d0/?retina=1',
 	srcset: null,
---
+++ resources/js/components/CaptionedImage.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="relative block overflow-hidden">
+		<img
+			class="relative mx-auto object-contain"
+			:src="src"
+			:alt="alt"
+			:title="title"
+			:srcset="srcset"
+		>
+		<figcaption
+			v-if="caption"
+			class="caption absolute bottom-0 left-0 block w-full p-4 text-center bg-black/30"
+		>
+			{{ caption }}
+		</figcaption>
+	</div>
+</template>
+
+<script setup lang="ts">
+type Url = string
+
+// TODO: figure out why I have to redefine this interface instead of importing
+interface ImageWithCaption {
+	alt?: string
+	srcset?: Url[]
+	src: Url
+	title?: string
+	caption?: string
+}
+
+const props = defineProps<ImageWithCaption>()
+</script>
