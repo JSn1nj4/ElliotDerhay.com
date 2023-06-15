a71f7f149e353efc6f4d5c58d38e6bbbb263e7fa
Elliot Derhay <elliot@elliotderhay.com>
2023-06-14 21:58:14 -0400

    Generate using spatie/laravel-sitemap
    Super easy solution, including automatically creating for models.

--- routes/dev.php
+++ routes/dev.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 // error page testing route (only works locally)
 Route::get('/error/{code}', function ($code = null) {
@@ -9,3 +13,27 @@ Route::get('/error/{code}', function ($code = null) {
 
 	abort($code);
 })->where('code', '[1-5][0-9]{2}');
+
+Route::get('/sitemap.xml',
+	static fn (Request $request) => Cache::remember('sitemap',
+		10,
+		static fn (): Sitemap => Sitemap::create()
+
+			->add(Url::create(route('home'))
+				->setLastModificationDate(Carbon::today())
+				->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+				->setPriority(0.1))
+
+			->add(Url::create(route('privacy'))
+				->setLastModificationDate(Carbon::today())
+				->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+				->setPriority(0.1))
+
+			->add(Url::create(route('blog'))
+				->setLastModificationDate(Carbon::today())
+				->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+				->setPriority(0.1))
+
+			->add(Post::all())
+		)
+		->toResponse($request));
