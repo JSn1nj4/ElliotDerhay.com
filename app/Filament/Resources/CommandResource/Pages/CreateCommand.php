4514c73fe727990660a15f4150e0b1eb0a8629b3
Elliot Derhay <elliot@elliotderhay.com>
2023-06-26 12:14:51 -0400

    Add a subheading to the Create Command view

--- app/Filament/Resources/CommandResource/Pages/CreateCommand.php
+++ app/Filament/Resources/CommandResource/Pages/CreateCommand.php
@@ -4,9 +4,12 @@ namespace App\Filament\Resources\CommandResource\Pages;
 
 use App\Filament\Resources\CommandResource;
 use Filament\Pages\Actions;
+use Filament\Resources\Form;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCommand extends CreateRecord
 {
     protected static string $resource = CommandResource::class;
+
+	protected string|null $subheading = "Register a new admin panel command.";
 }
