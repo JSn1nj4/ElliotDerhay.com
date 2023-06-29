7f9a04458a16190b93cb57d29d9e87a322c05b49
Elliot Derhay <elliot@elliotderhay.com>
2023-06-28 22:54:41 -0400

    Dashboard logo

---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,6 @@
+<a href="/" class="text-white p-2">
+	<img src="{{ asset_url("avatar.png") }}" width="60" height="60" alt="ElliotDerhay.com logo" title="Elliot Derhay" class="inline border-solid border-2 border-white rounded-full align-middle">
+	<span class="text-xl sm:text-3xl tracking-tighter py-px2 pl-2 align-middle">
+		Elliot Derhay
+	</span>
+</a>
