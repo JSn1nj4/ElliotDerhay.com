8e3a8e403423e3764b848e7df3e35f2e67c4ef59
Elliot Derhay <elliot@elliotderhay.com>
2023-08-11 23:10:34 -0400

    Trim ListImages view

--- app/Filament/Resources/ImageResource/Pages/ListImages.php
+++ app/Filament/Resources/ImageResource/Pages/ListImages.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\ImageResource\Pages;
 
 use App\Filament\Resources\ImageResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListImages extends ListRecords
 {
     protected static string $resource = ImageResource::class;
-
-    protected function getActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
