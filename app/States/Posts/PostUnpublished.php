44ed7050d8b0ea57da1c5a915c3a23a119b34e04
Elliot Derhay <elliot@elliotderhay.com>
2023-09-19 22:59:57 -0400

    Schedule tweeting new post

--- app/States/Posts/PostUnpublished.php
+++ app/States/Posts/PostUnpublished.php
@@ -2,20 +2,24 @@
 
 namespace App\States\Posts;
 
+use App\Jobs\PostToXJob;
+
 class PostUnpublished extends PostState
 {
 	/**
 	 * @return bool
 	 * @throws \Exception
 	 */
-    public function publish(): bool
+	public function publish(): bool
 	{
 		$this->post->published = true;
 
 		if (!$this->post->published_at) {
 			$this->post->published_at = now();
+
+			PostToXJob::dispatch($this->post->getPostable());
 		}
 
 		return $this->post->save() || parent::publish();
-    }
+	}
 }
