d758187e1e9094b299a23323dbd424908ff8a517
JSn1nj4 <elliot@elliotderhay.com>
2024-02-08 23:04:47 -0500

    Switch to fully local dev (#176)
    * Ignore error log
    
    * Environment stuff for cert files
    
    * purge pm2
    
    * Update startup scripts
    
    * Update CHANGELOG

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,49 @@
-import {defineConfig, HmrOptions, loadEnv, ServerOptions, UserConfig} from 'vite'
+import {
+	defineConfig,
+	HmrOptions,
+	loadEnv,
+	ServerOptions,
+	UserConfig,
+} from 'vite'
 import laravel from 'laravel-vite-plugin'
-import vuePlugin from "@vitejs/plugin-vue"
-import * as fs from "fs"
+import vuePlugin from '@vitejs/plugin-vue'
+import * as fs from 'fs'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
 	// setup
 	Object.assign(process.env, loadEnv(mode, process.cwd()))
 	const {
+		VITE_CERT,
+		VITE_CERT_KEY,
 		VITE_SERVER_HOST,
 		VITE_SERVER_HMR_HOST,
 		VITE_SERVER_HTTPS,
-		VITE_SERVER_PORT
+		VITE_SERVER_PORT,
 	} = process.env
 
 	// build hmr config
 	const hmr: HmrOptions = {}
-	if(VITE_SERVER_HMR_HOST) hmr.host = VITE_SERVER_HMR_HOST
+	if (VITE_SERVER_HMR_HOST) hmr.host = VITE_SERVER_HMR_HOST
 
 	// build vite server config
 	const server: ServerOptions = {}
-	if(Object.keys(hmr).length > 0) server.hmr = hmr
-	if(VITE_SERVER_HOST) server.host = VITE_SERVER_HOST !== 'true' ? VITE_SERVER_HOST : true
-	if(VITE_SERVER_HTTPS) server.https = VITE_SERVER_HTTPS === 'true' ? {
-		cert: fs.readFileSync('/certs/cert.crt'),
-		key: fs.readFileSync('/certs/cert.key'),
-    } :  false
+	if (Object.keys(hmr).length > 0) server.hmr = hmr
+	if (VITE_SERVER_HOST)
+		server.host = VITE_SERVER_HOST !== 'true' ? VITE_SERVER_HOST : true
+	if (VITE_SERVER_HTTPS)
+		server.https =
+			VITE_SERVER_HTTPS === 'true'
+				? {
+						cert: fs.readFileSync(VITE_CERT),
+						key: fs.readFileSync(VITE_CERT_KEY),
+				  }
+				: false
 	server.port = VITE_SERVER_PORT ? parseInt(VITE_SERVER_PORT) : 24690
 
 	// build viteConfig object
 	const viteConfig: UserConfig = {}
-	if(Object.keys(server).length > 0) viteConfig.server = server
+	if (Object.keys(server).length > 0) viteConfig.server = server
+
 	viteConfig.plugins = [
 		laravel({
 			input: [
@@ -37,7 +51,7 @@ export default defineConfig(({ mode }) => {
 				'resources/js/admin/app.ts',
 				'resources/js/app.ts',
 			],
-			refresh: true
+			refresh: true,
 		}),
 		vuePlugin(),
 	]
