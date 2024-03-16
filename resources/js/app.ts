2e62be1a356ed87f2defaa031ed4a3a349470bde
Elliot Derhay <elliot@elliotderhay.com>
2024-03-16 15:25:56 -0400

    Convince TS new `window` props are ok

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,8 +6,15 @@ import {createApp} from 'vue'
 import Lightbox from './Lightbox.vue'
 import Alpine from '@alpinejs/csp'
 
+// convince TS that adding new stuff to `window` is ok
+declare global {
+	interface Window {
+		Alpine: any
+	}
+}
+
 createApp(Lightbox).mount('#lightbox-modal')
 
-window.Alpine = Alpine
+window.Alpine = Alpine || {}
 
 Alpine.start()
