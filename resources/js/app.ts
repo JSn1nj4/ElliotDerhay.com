31bb3be98e69f30fe10a50ea749b5b9ffb6b81ae
Elliot Derhay <elliot@elliotderhay.com>
2024-03-16 17:07:59 -0400

    Lightbox is now in Alpine!

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,8 +2,6 @@
  * Popup idea inspiration:
  * - https://kevdees.com/adding-google-analytics-to-your-website-while-respecting-do-not-track/
  */
-import {createApp} from 'vue'
-import Lightbox from './Lightbox.vue'
 import Alpine from '@alpinejs/csp'
 
 // convince TS that adding new stuff to `window` is ok
@@ -12,8 +10,6 @@ declare global {
 		Alpine: any
 	}
 }
-
-createApp(Lightbox).mount('#lightbox-modal')
 
 window.Alpine = Alpine || {}
 
