726e1d65667b34ccd6918a0b454fa08fe76277b0
Elliot Derhay <elliot@elliotderhay.com>
2024-06-15 19:27:10 -0400

    Create basic constructors
    Still need to configure how credentials will work though

--- app/Services/Mastodon/MastodonClient.php
+++ app/Services/Mastodon/MastodonClient.php
@@ -2,7 +2,11 @@
 
 namespace App\Services\Mastodon;
 
+use App\DataTransferObjects\MastodonApiCredentials;
+
 class MastodonClient
 {
-
+	public function __construct(
+		#[\SensitiveParameter] protected MastodonApiCredentials $credentials,
+	) {}
 }
--- app/Services/Mastodon/MastodonService.php
+++ app/Services/Mastodon/MastodonService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Mastodon;
 
 use App\Contracts\SocialMediaService;
+use App\DataTransferObjects\MastodonApiCredentials;
 use App\DataTransferObjects\OperationResult;
 use App\DataTransferObjects\SocialPostDTO;
 use Illuminate\Support\Collection;
@@ -10,6 +11,11 @@ use Illuminate\Support\Collection;
 class MastodonService implements SocialMediaService
 {
 	protected MastodonClient $client;
+
+	public function __construct(#[\SensitiveParameter] MastodonApiCredentials $credentials)
+	{
+		$this->client = new MastodonClient($credentials);
+	}
 
 	public function getPosts(string $username, ?string $since = null, bool $reposts = true, ?int $count = null): Collection
 	{
