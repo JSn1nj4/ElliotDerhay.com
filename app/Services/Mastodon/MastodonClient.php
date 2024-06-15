c0a13ef01b8617fb74bf0b5901df994577cf6261
Elliot Derhay <elliot@elliotderhay.com>
2024-06-15 19:18:44 -0400

    Create client and service skeletons

---
+++ app/Services/Mastodon/MastodonClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Mastodon;
+
+class MastodonClient
+{
+
+}
---
+++ app/Services/Mastodon/MastodonService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\Mastodon;
+
+use App\Contracts\SocialMediaService;
+use App\DataTransferObjects\OperationResult;
+use App\DataTransferObjects\SocialPostDTO;
+use Illuminate\Support\Collection;
+
+class MastodonService implements SocialMediaService
+{
+	protected MastodonClient $client;
+
+	public function getPosts(string $username, ?string $since = null, bool $reposts = true, ?int $count = null): Collection
+	{
+		// TODO: Implement getPosts() method.
+	}
+
+	public function post(SocialPostDTO $postDTO): OperationResult
+	{
+		// TODO: Implement post() method.
+	}
+}
