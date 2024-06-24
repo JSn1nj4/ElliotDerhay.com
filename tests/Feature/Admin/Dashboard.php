29c5f932a30ddf27ffcd010a1c26d2866033a264
Elliot Derhay <elliot@elliotderhay.com>
2024-06-24 13:55:42 -0400

    Full reset on testing
    Hadn't kept up on testing. Now too many tests are either failing for unknown reasons or useless at this point.
    
    Best to start over with what's actively being built.
    
    Stale datasets will be pruned in the future.

--- tests/Feature/Admin/Dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-it('allows loading the dashboard when logged in', function () {
-	$this->actingAs(createUser())
-		->get('/admin')
-		->assertStatus(200);
-});
--- tests/Feature/Public/PrivacyPageTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-it('loads the privacy page', function () {
-	$this->get(route('privacy'))->assertStatus(200);
-});
