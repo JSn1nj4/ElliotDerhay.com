91c19f72e8bb64a95a97697151ef75a373bc095d
Elliot Derhay <elliot@elliotderhay.com>
2024-07-07 16:20:09 -0400

    Test mystery #4 solved
    Was complaining about not being able to drop indexes because they didn't exist, meaning it was happening when running migration `down()` methods.
    
    Now these migrations conditionally check if the indexes exist before trying to drop them.

--- database/migrations/2023_05_31_115217_add_slug_to_tags.php
+++ database/migrations/2023_05_31_115217_add_slug_to_tags.php
@@ -4,26 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
 		Schema::whenTableDoesntHaveColumn('tags', 'slug', static function (Blueprint $table) {
 			$table->string('slug')->unique();
 		});
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::whenTableHasColumn('tags', 'slug', static function (Blueprint $table) {
-			$table->dropIndex('slug');
-            $table->removeColumn('slug');
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::whenTableHasColumn('tags', 'slug', static function (Blueprint $table) {
+			if (Schema::hasIndex('categories', 'slug')) {
+				$table->dropIndex('slug');
+			};
+
+			$table->removeColumn('slug');
+		});
+	}
 };
