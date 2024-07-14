b80583c52e013f646316a9596f819fa58f95e95c
Elliot Derhay <elliot@elliotderhay.com>
2024-07-14 18:27:22 -0400

    Unwrap old scheduled Twitter commands
    The condition used to schedule these is being checked inside the commands themselves anyway, so they'll skip running automatically.

--- routes/console.php
+++ routes/console.php
@@ -6,20 +6,16 @@ use App\Console\Commands\{GithubEventPullCommand,
 	TokenPruneCommand,
 	TweetPullCommand,
 	TwitterUserUpdateCommand};
-use App\Features\TwitterFeed;
 use App\Jobs\CleanTempStorageJob;
 use Illuminate\Queue\Console\WorkCommand;
 use Illuminate\Support\Facades\Schedule;
-use Laravel\Pennant\Feature;
 
 Schedule::command(GithubEventPullCommand::class)->hourly();
 Schedule::command(GithubUserUpdateCommand::class)->weekly();
 Schedule::command(TokenPruneCommand::class)->daily();
 
-if (Feature::active(TwitterFeed::class)) {
-	Schedule::command(TweetPullCommand::class)->hourly();
-	Schedule::command(TwitterUserUpdateCommand::class)->weekly();
-}
+Schedule::command(TweetPullCommand::class)->hourly();
+Schedule::command(TwitterUserUpdateCommand::class)->weekly();
 
 Schedule::job(CleanTempStorageJob::class)->weekly();
 Schedule::command(WorkCommand::class, ['--stop-when-empty'])->daily();
