0b6f59fcbd4c72edce97c36f7f1e84621ce35d17
Elliot Derhay <elliot@elliotderhay.com>
2024-08-02 23:25:05 -0400

    Update model helpers

--- app/Models/LoginActivity.php
+++ app/Models/LoginActivity.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $email
+ * @property int $succeeded
+ * @property string|null $info
+ * @property string $ip_address
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity failed()
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity succeeded()
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereInfo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereIpAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereSucceeded($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LoginActivity whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class LoginActivity extends Model
 {
 	use HasFactory;
@@ -22,4 +47,14 @@ class LoginActivity extends Model
 		'info',
 		'ip_address',
 	];
+
+	public function scopeFailed(Builder $query): void
+	{
+		$query->where('succeeded', false);
+	}
+
+	public function scopeSucceeded(Builder $query): void
+	{
+		$query->where('succeeded', true);
+	}
 }
