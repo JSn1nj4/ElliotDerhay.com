0598b42450266c5572d5b59b731ac08a21676f44
Elliot Derhay <elliot@elliotderhay.com>
2024-12-08 15:47:04 -0500

    Start a Mastodon connection status view

--- app/Filament/Pages/SocialMediaConnections.php
+++ app/Filament/Pages/SocialMediaConnections.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Widgets\MastodonConnectionWidget;
 use Filament\Pages\Page;
 
 class SocialMediaConnections extends Page
@@ -11,5 +12,19 @@ class SocialMediaConnections extends Page
 
 	protected static int|null $navigationSort = 3;
 
+	protected static string|null $slug = 'social-media';
+
 	protected static string $view = 'filament.pages.social-media-connections';
+
+	protected function getHeaderWidgets(): array
+	{
+		return [
+			MastodonConnectionWidget::class,
+		];
+	}
+
+	public function getHeaderWidgetsColumns(): int|string|array
+	{
+		return 3;
+	}
 }
