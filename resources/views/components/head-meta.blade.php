c1a114101710271f4b3520557d9ebbf8a0f0644d
Elliot Derhay <elliot@elliotderhay.com>
2025-02-05 13:48:51 -0500

    Fix meta component

--- resources/views/components/head-meta.blade.php
+++ resources/views/components/head-meta.blade.php
@@ -1,9 +1,7 @@
-<template>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="theme-color" content="#002e24">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#002e24">
 
-	<title>{{ $title }}</title>
-	<meta name="description" content="{{ $description }}">
-</template>
+<title>{{ $title }}</title>
+<meta name="description" content="{{ $description }}">
