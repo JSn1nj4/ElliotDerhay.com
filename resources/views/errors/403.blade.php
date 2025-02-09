f1b70cd2d9336483f9946609f20b29fee2052734
Elliot Derhay <elliot@elliotderhay.com>
2025-02-09 06:58:46 -0500

    Update error messages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,21 +4,22 @@
 ])
 
 @section('status-body')
-		<p>
-				Sorry, but you're not authorized to view this resource.
-		</p>
+	<p>
+		Sorry, but you're not authorized to view this.
+	</p>
 @endsection
 
 @section('status-footer')
-		<p>
-				<a class="text-black dark:text-white hover:text-caribbeanGreen-500" href="{{ route('home') }}"><i class="text-caribbeanGreen-500 fa fa-caret-square-left"></i> Back to homepage</a>
-		</p>
-		<div class="pt-8 font-normal text-neutral-500">
-			@include('partials.copyright')
-		</div>
-		<div class="pt-4">
-			@include('partials.socials', [
-				'classes' => 'text-2xl'
-			])
-		</div>
+	<p>
+		<a class="text-black dark:text-white hover:text-caribbeanGreen-500" href="{{ route('home') }}"><i
+				class="text-caribbeanGreen-500 fa fa-caret-square-left"></i> Back to homepage</a>
+	</p>
+	<div class="pt-8 font-normal text-neutral-500">
+		@include('partials.copyright')
+	</div>
+	<div class="pt-4">
+		@include('partials.socials', [
+			'classes' => 'text-2xl'
+		])
+	</div>
 @endsection
