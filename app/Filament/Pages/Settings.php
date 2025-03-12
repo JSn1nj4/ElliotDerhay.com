e4036cc0424421645ed91a8c949ba40f5c21af4d
Elliot Derhay <elliot@elliotderhay.com>
2025-03-11 22:36:27 -0400

    Add layout for feature settings

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Forms;
 use Filament\Pages\Page;
 
 class Settings extends Page
@@ -13,4 +14,24 @@ class Settings extends Page
 	protected static string|null $navigationGroup = 'Administration';
 
 	protected static string|null $navigationLabel = 'Settings';
+
+	public function form(Forms\Form $form): Forms\Form
+	{
+		return $form->schema([
+			Forms\Components\Section::make('Features')
+				->schema([
+					Forms\Components\Toggle::make('admin_login')
+						->label('Admin Login')
+						->inlineLabel(),
+
+					Forms\Components\Toggle::make('github_feed')
+						->label('GitHub Activity Feed')
+						->inlineLabel(),
+
+					Forms\Components\Toggle::make('publish_posts_to_x')
+						->label('Publish Posts to X')
+						->inlineLabel(),
+				])
+		]);
+	}
 }
