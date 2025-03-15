7d4c9b715083b8239d72bcb71636dbdeb2ea67dd
Elliot Derhay <elliot@elliotderhay.com>
2025-03-15 06:39:56 -0400

    Fix GitHub event item styling

--- resources/views/components/github/event-icon.blade.php
+++ resources/views/components/github/event-icon.blade.php
@@ -1,4 +1,3 @@
-<div class="text-neutral-800 dark:text-neutral-500 text-center flex-none"
-		 style="width: 2rem; font-size: 22px;">
-	<x-dynamic-component :component='$icon' class='size-4' />
+<div class="text-neutral-800 dark:text-neutral-500 text-center flex-none w-8 flex justify-center">
+	<x-dynamic-component :component='$icon' class='size-6' />
 </div>
