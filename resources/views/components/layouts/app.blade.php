8c2d844f4c98c898cd13d1ef3a36826bc89dc337
Elliot Derhay <elliot@elliotderhay.com>
2025-03-15 08:19:17 -0400

    Remove last direct FontAwesome script dependency

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,6 @@
 	/>
 
 	@vite('resources/css/app.css')
-	<script src="https://kit.fontawesome.com/a9f488e9e4.js" crossorigin="anonymous"></script>
 
 	<link rel="shortcut icon" href="{{ asset_url('favicon.png') }}">
 
