d15b37236e9cec2c1652fae415bf18acde82d544
Elliot Derhay <elliot@elliotderhay.com>
2025-05-03 16:55:45 -0400

    WIP - fixing PrunLoginActivityJob tests

--- tests/Feature/Jobs/PruneLoginActivityJobTest.php
+++ tests/Feature/Jobs/PruneLoginActivityJobTest.php
@@ -2,39 +2,84 @@
 
 use App\Jobs\PruneLoginActivityJob;
 use App\Models\LoginActivity;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
-it('prunes old login activity', function () {
-	$removed_count = LoginActivity::factory(fake()->randomDigitNot(0))
-			->state(['created_at' => now()->subDays(
-				config('auth.activity.days_to_keep') + 1,
-			)])
-			->create()->count()
+it('keeps all entries when below minimum number', function () {
+	/**
+	 * this can be tested simply by
+	 * - creating the minimum number of entries and making sure none get pruned, and
+	 * - creating less than the minimum and making sure none of them get pruned
+	 */
 
-		+ LoginActivity::factory(fake()->randomDigitNot(0))
-			->state(['created_at' => now()->subDays(
-				config('auth.activity.days_to_keep'),
-			)])
-			->create()->count();
+	$minimum = config()->integer('auth.activity.min_entries');
 
-	$remaining_count = LoginActivity::factory(fake()->randomDigitNot(0))
-		->state(['created_at' => now()->subDays(
-			config('auth.activity.days_to_keep') - 1,
-		)])
-		->create()->count();
+	$createdCount = LoginActivity::factory($minimum)->create()->count();
 
-	expect(LoginActivity::count())
-		->toEqual($removed_count + $remaining_count)
-		->toBeGreaterThan($remaining_count);
+	expect($createdCount)->toEqual($minimum, "started with {$minimum} entries");
 
 	PruneLoginActivityJob::dispatchSync();
 
-	$activity = LoginActivity::all();
-
-	expect($activity->count())
-		->toEqual($remaining_count)
-		->toBeLessThan($removed_count + $remaining_count);
-
-	$activity->each(fn (LoginActivity $item) => assert(now()
-		->subDays(config('auth.activity.days_to_keep'))
-		->lessThan($item->created_at)));
+	expect(LoginActivity::count())->toEqual($minimum, "activity count is unchanged");
 });
+
+it('keeps all entries younger than the max age', function () {
+	/**
+	 * this can be tested by simply
+	 * - creating more entries than the minimum number
+	 * - making sure none of them get pruned
+	 */
+
+	$minimum = config()->integer('auth.activity.min_entries');
+
+	$createdCount = LoginActivity::factory($minimum + 10)->create()->count();
+
+	expect($createdCount)
+		->toBeGreaterThan($minimum)
+		->toEqual($createdCount, "started with {$createdCount} entries");
+
+	PruneLoginActivityJob::dispatchSync();
+
+	expect(LoginActivity::count())
+		->toBeGreaterThan($minimum)
+		->toEqual($createdCount, "activity count is unchanged");
+});
+
+it('prunes entries older than max age', function () {
+	$minimum = config()->integer('auth.activity.min_entries');
+
+	$testFloor = $minimum * 2;
+	$testCeiling = $testFloor + fake()->randomNumber(4);
+
+	$now = now();
+	$oldAge = now()->startOfDay()->subDays(config()->integer('auth.activity.days_to_retain') + 1);
+
+	$createdCount = LoginActivity::factory(random_int($testFloor, $testCeiling))
+		->state(new Sequence(
+			['created_at' => $now],
+			['created_at' => $now],
+			['created_at' => $now],
+			['created_at' => $oldAge],
+		))
+		->create()
+		->count();
+
+	expect(LoginActivity::count())
+		->toBeGreaterThan($minimum)
+		->toEqual($createdCount, "started with {$createdCount} entries");
+
+	$countToRemove = LoginActivity::old()->count();
+
+	PruneLoginActivityJob::dispatchSync();
+
+	expect(LoginActivity::count())
+		->toBeLessThan($createdCount)
+		->toEqual($createdCount - $countToRemove);
+})->wip();
+
+it('keeps minimum number of entries when \'minimum days worth\' rule won\'t retain enough', function () {
+	/**
+	 * this can be tested by doing 2 things
+	 * - creating slightly more entries than the minimum number
+	 * - setting half of the entries to older than the max age
+	 */
+})->wip();
