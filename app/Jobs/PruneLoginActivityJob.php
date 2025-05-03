a8a7c88c62bef99f2146983fad6216d7f235170b
Elliot Derhay <elliot@elliotderhay.com>
2025-05-03 12:04:24 -0400

    Make PruneLoginActivityJob conform to new requirements
    - only prune older than X days
    - prune only if above minimum threshold

--- app/Jobs/PruneLoginActivityJob.php
+++ app/Jobs/PruneLoginActivityJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\LoginActivity;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -12,19 +13,30 @@ class PruneLoginActivityJob implements ShouldQueue
 {
 	use Dispatchable, InteractsWithQueue, Queueable;
 
-	public function __construct() {}
+	protected Builder $baseQuery;
 
 	public function handle(): void
 	{
-		if (LoginActivity::count() <= config()->integer('auth.activity.min_entries')) {
-			return;
-		}
+		$this->baseQuery = LoginActivity::old();
 
-		LoginActivity::whereDate(
-			'created_at',
-			'<=',
-			now()->startOfDay()
-				->subDays(config('auth.activity.days_to_keep'))
-		)->delete();
+		$totalToRemove = $this->totalToRemove();
+
+		if ($totalToRemove <= 0) return;
+
+		$this->baseQuery->oldest()->take($totalToRemove)->delete();
+	}
+
+	protected function totalToRemove(): int
+	{
+		$total = LoginActivity::count();
+		$minimum = config()->integer('auth.activity.min_entries');
+
+		// small optimization - avoid more queries
+		if ($total <= $minimum) return 0;
+
+		$agedOutTotal = $this->baseQuery->clone()->count();
+
+		// ensure to only reduce the amount to remove if the 'old' count overlaps the minimum number
+		return $agedOutTotal - max($total - $agedOutTotal - $minimum, 0);
 	}
 }
