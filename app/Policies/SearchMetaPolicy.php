5d0724eaf531236a63d9995118fd6d0ada247126
Elliot Derhay <elliot@elliotderhay.com>
2025-05-20 23:12:15 -0400

    Fix common issue across all policies
    This is the same issue that was present on the PostPolicy: calling `->exists()` instead of checking `$exists` on the model.

--- app/Policies/SearchMetaPolicy.php
+++ app/Policies/SearchMetaPolicy.php
@@ -4,63 +4,62 @@ namespace App\Policies;
 
 use App\Models\SearchMeta;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class SearchMetaPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return $user->exists();
-    }
+	/**
+	 * Determine whether the user can view any models.
+	 */
+	public function viewAny(User $user): bool
+	{
+		return $user->exists;
+	}
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, SearchMeta $searchMeta): bool
-    {
-        return $user->exists() && $searchMeta->exists();
-    }
+	/**
+	 * Determine whether the user can view the model.
+	 */
+	public function view(User $user, SearchMeta $searchMeta): bool
+	{
+		return $user->exists && $searchMeta->exists;
+	}
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return $user->exists();
-    }
+	/**
+	 * Determine whether the user can create models.
+	 */
+	public function create(User $user): bool
+	{
+		return $user->exists;
+	}
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, SearchMeta $searchMeta): bool
-    {
-		return $user->exists() && $searchMeta->exists();
-    }
+	/**
+	 * Determine whether the user can update the model.
+	 */
+	public function update(User $user, SearchMeta $searchMeta): bool
+	{
+		return $user->exists && $searchMeta->exists;
+	}
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, SearchMeta $searchMeta): bool
-    {
-		return $user->exists() && $searchMeta->exists();
-    }
+	/**
+	 * Determine whether the user can delete the model.
+	 */
+	public function delete(User $user, SearchMeta $searchMeta): bool
+	{
+		return $user->exists && $searchMeta->exists;
+	}
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, SearchMeta $searchMeta): bool
-    {
-		return $user->exists() && $searchMeta->exist()();
-    }
+	/**
+	 * Determine whether the user can restore the model.
+	 */
+	public function restore(User $user, SearchMeta $searchMeta): bool
+	{
+		return $user->exists && $searchMeta->exist()();
+	}
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, SearchMeta $searchMeta): bool
-    {
-		return $user->exists() && $searchMeta->exists();
-    }
+	/**
+	 * Determine whether the user can permanently delete the model.
+	 */
+	public function forceDelete(User $user, SearchMeta $searchMeta): bool
+	{
+		return $user->exists && $searchMeta->exists;
+	}
 }
