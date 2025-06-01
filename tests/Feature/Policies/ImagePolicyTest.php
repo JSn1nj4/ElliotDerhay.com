77dc11971649f7d60fb3a24a596a4df484f8f925
Elliot Derhay <elliot@elliotderhay.com>
2025-06-01 15:07:17 -0400

    Finish ImagePolicyTest

---
+++ tests/Feature/Policies/ImagePolicyTest.php
@@ -0,0 +1,135 @@
+<?php
+
+use App\Models\Image;
+use App\Policies\ImagePolicy;
+
+/** @todo replace real image generating process with fake image data for policy testing */
+
+it('can be listed by any user', fn () => expect(new ImagePolicy())
+	->viewAny(makeUser())
+	->toBeTrue());
+
+it('can be viewed by any user', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->view(makeUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeTrue();
+});
+
+it('cannot be created by anonymous users', fn () => expect(new ImagePolicy())
+	->create(makeUser())
+	->toBeFalse());
+
+it('can be created by a logged-in user', fn () => expect(new ImagePolicy())
+	->create(createUser())
+	->toBeTrue());
+
+it('cannot be updated by anonymous users', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->update(makeUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeFalse();
+});
+
+it('can be updated by a logged-in user', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->update(createUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeTrue();
+});
+
+it('cannot be deleted by anonymous users', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->delete(makeUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeFalse();
+});
+
+it('can be deleted by a logged-in user', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->delete(createUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeTrue();
+});
+
+it('cannot be restored by anonymous users', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->restore(makeUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeFalse();
+});
+
+it('can be restored by a logged-in user', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->restore(createUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeTrue();
+});
+
+it('cannot be force-deleted by anonymous users', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->forceDelete(makeUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeFalse();
+});
+
+it('can be force-deleted by a logged-in user', function () {
+	$diskName = 'public';
+	$disk = Storage::fake($diskName);
+
+	Storage::fake('temp');
+
+	expect(new ImagePolicy())
+		->forceDelete(createUser(), Image::factory()
+			->setDisk($diskName, $disk)
+			->createOne())
+		->toBeTrue();
+});
