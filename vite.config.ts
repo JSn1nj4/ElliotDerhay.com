7e1f1da8f3288036e5aab64739c635d31fcc61f7
JSn1nj4 <elliot@elliotderhay.com>
2025-06-07 10:13:06 -0400

    JSN-306: Clear view and config caches on file save (#246)
    * Watch Renderers for changes for page reload
    
    * Start tracking some .idea stuff
    
    * Update CHANGELOG

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import {defineConfig, loadEnv} from 'vite'
-import laravel from 'laravel-vite-plugin'
+import laravel, {refreshPaths} from 'laravel-vite-plugin'
 import vuePlugin from '@vitejs/plugin-vue'
 import * as fs from 'fs'
 import {SecureServerOptions} from 'node:http2'
@@ -11,7 +11,11 @@ export default defineConfig(({mode}) => {
 		plugins: [
 			laravel({
 				input: ['resources/css/app.css', 'resources/js/app.ts'],
-				refresh: true,
+				refresh: [
+					{
+						paths: refreshPaths.concat(['app/View/Renderers/**']),
+					},
+				],
 			}),
 			vuePlugin(),
 		],
