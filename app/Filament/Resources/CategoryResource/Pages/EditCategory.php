54ed23164aaf01c8a5487fb2766da253a637dec7
Elliot Derhay <elliot@elliotderhay.com>
2025-08-13 22:07:05 -0400

    Add delete confirmation to category edit screen

--- app/Filament/Resources/CategoryResource/Pages/EditCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -8,12 +8,15 @@ use Filament\Resources\Pages\EditRecord;
 
 class EditCategory extends EditRecord
 {
-    protected static string $resource = CategoryResource::class;
+	protected static string $resource = CategoryResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
+	protected function getHeaderActions(): array
+	{
+		return [
+			Actions\DeleteAction::make()
+				->requiresConfirmation()
+				->modalDescription('This will remove the category from all attached items. Are you sure you want to continue?')
+				->databaseTransaction(),
+		];
+	}
 }
