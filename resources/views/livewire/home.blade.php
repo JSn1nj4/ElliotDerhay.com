e6d85601b3e6505df8d900baabfc2dc49fa50ce9
Elliot Derhay <elliot@elliotderhay.com>
2025-11-02 15:40:12 -0500

    Simplify home content and design

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -7,80 +7,65 @@ new class extends Component {
 }; ?>
 
 <root>
-	<x-row id="about" overlay-classes="bg-caribbean-green-200/50 dark:bg-caribbean-green-700/75"
-				 background-image="{{ asset_url('pexels-lukas-574073-scaled.jpg') }}">
-		<x-column class="flex md:order-2 md:w-1/2 lg:w-5/12 xl:w-1/3">
+	<x-row id="about" flex-class="flex flex-row flex-wrap">
+		<x-column class="flex md:w-1/2 lg:w-5/12 xl:w-1/3">
 			<img src="{{ asset_url("Elliot-head-on-square-color-medium.jpg") }}" title="Elliot Derhay"
 					 alt="Photo of Elliot Derhay"
-					 class="border-black dark:border-white border-2 border-opacity-40 rounded-full">
+					 class="border-black dark:border-white border-2 border-opacity-40 rounded-3xl">
 		</x-column>
 
-		<x-column class="md:order-1 md:w-1/2 lg:w-7/12 xl:w-2/3">
-			<h1 class="content-title text-4xl pt-6 mt-4 md:mt-0 md:pt-0 text-center md:text-left">
-				Who I Am
-			</h1>
+		<x-column class="md:order-first md:w-1/2 lg:w-7/12 xl:w-2/3">
+			<div class='flex flex-col gap-4'>
+				<h1
+					class="text-2xl pt-6 mt-4 md:mt-0 md:pt-0 uppercase">
+					About me
+				</h1>
 
-			<p class="mb-4 text-xl">
-				I'm a fairly simple guy who loves web development and design. I love learning new software and web development
-				concepts. I like learning in general, but especially about science and technology.
-			</p>
+				<p class='text-3xl'>
+					Hey, there! I'm Elliot Derhay.
+				</p>
 
-			<p class="mb-4 text-xl">
-				When I'm not spending time learning something new, you can probably find me reading tech news and current
-				events, watching videos by channels like Veritasium and SmarterEveryDay, reading comics like Dilbert and xkcd,
-				playing card games, or playing puzzle games like Sudoku.
-			</p>
-		</x-column>
-	</x-row>
+				<p class="text-xl">
+					I'm a web developer. I work primarily with WordPress and squeeze in a Laravel project whenever I can. I also
+					dream up more ideas than I have time to actually work on.
+				</p>
 
-	<x-row :flex-class="null" id="projects">
-		<x-column>
-			<h2 class="content-title text-4xl pt-6 mt-5 md:mt-0 md:pt-0">
-				What I Do
-			</h2>
+				<p class="text-xl">
+					I also love blogging — the part where I hammer out words before remembering that tech blogging needs
+					supporting
+					examples.
+				</p>
 
-			<p class="mb-8 text-xl">
-				I primarily work on WordPress projects, but I love working with Laravel. My primary side project is this
-				website. I use it as both a personal profile and a sandbox for learning new
-				concepts. {{-- Below are some projects I maintain or contribute to, as well as some I have worked on in the past. --}}
-			</p>
+				<p class="text-xl">
+					You can find also find me on Twitter/X sometimes Tweeting/Xeeting/posting into the Ether, and reading about
+					what's going on in tech and the broader world. I also used to blog on dev.to occasionally.
+				</p>
 
-			<x-project.grid :count="3" />
-		</x-column>
-	</x-row>
-
-	<x-row :flex-class="null" class="bg-caribbean-green-200/40 dark:bg-caribbean-green-500/25"
-				 id="connect">
-		<x-column class="block w-full">
-			<h2 class="content-title text-4xl pt-6 mt-5 md:mt-0 md:pt-0">
-				What I'm Up To
-			</h2>
-
-			<p class="mb-4 text-xl">
-				I can usually be found on Twitter and GitHub. I've also been known to occasionally write and comment on <a
-					href="https://dev.to/jsn1nj4" target="_blank">dev.to</a> articles.
-			</p>
+				<p class="text-xl">
+					I'm also a Bible-believing Christian, husband, and father. And I love playing board and card games when time
+					allows.
+				</p>
+			</div>
 		</x-column>
 
-		<div class="block md:flex">
-			@feature(App\Features\BlogIndex::class)
-			<x-column class="block md:w-1/2">
-				<h2 class="content-title text-2xl pt-6 mt-4 text-center">Latest Blog Post</h2>
-				<section id="blog_feed-home">
-					<x-post.latest />
-				</section>
-			</x-column>
-			@endfeature
+		@feature(App\Features\BlogIndex::class)
+		<x-column class="block md:w-1/2">
+			<h2 class="content-title text-2xl pt-6 mt-4 uppercase">
+				Latest Blog Post</h2>
+			<section id="blog_feed-home">
+				<x-post.latest />
+			</section>
+		</x-column>
+		@endfeature
 
-			@feature(App\Features\GithubFeed::class)
-			<x-column class="block md:w-1/2">
-				<h2 class="content-title text-2xl pt-6 mt-4 text-center">GitHub Activity</h2>
-				<section id="github_events_feed-home" class="font-mono">
-					<x-github.events-feed padding="px-4 py-6" count="5" />
-				</section>
-			</x-column>
-			@endfeature
-		</div>
-
+		@feature(App\Features\GithubFeed::class)
+		<x-column class="block md:w-1/2">
+			<h2 class="content-title text-2xl pt-6 mt-4 uppercase">
+				GitHub Activity</h2>
+			<section id="github_events_feed-home" class="font-mono">
+				<x-github.events-feed padding="px-4 py-6" count="5" />
+			</section>
+		</x-column>
+		@endfeature
 	</x-row>
 </root>
