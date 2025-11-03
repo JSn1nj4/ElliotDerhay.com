60ffacd2032e1a3be8ab32e4c6756e561d648404
Elliot Derhay <elliot@elliotderhay.com>
2025-11-02 21:44:44 -0500

    Start prepping for a custom variant

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header"
-				class="flex-initial border-b-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500 dark:bg-black/50">
+				class="flex-initial border-b-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500 dark:bg-black/50 transition-[border-width] fun:border-0">
 	<div class="container m-auto px-4">
 
 		{{ $slot }}
