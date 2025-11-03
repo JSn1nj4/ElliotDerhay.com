e03b1bc93cbddb31ea10d8172ccdf873d2d954b8
Elliot Derhay <elliot@elliotderhay.com>
2025-11-02 21:04:33 -0500

    Make header and footer same width as body

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer
 	class="dark:bg-black/60 flex-initial border-t-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500">
-	<div class="container-flexible-large w-full m-auto bottom-0 p-3">
+	<div class="container w-full m-auto bottom-0 px-4 py-3">
 		<div class="p-6 text-center text-bright-turquoise-900 dark:text-bright-turquoise-500">
 			@include('partials.copyright')
 		</div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header id="header"
 				class="flex-initial border-b-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500 dark:bg-black/50">
-	<div class="container-flexible-large m-auto">
+	<div class="container m-auto px-4">
 
 		{{ $slot }}
 
