0d90b9004fba71b0a1e70dedd31a185d8e8d681f
Elliot Derhay <elliot@elliotderhay.com>
2025-11-04 08:18:36 -0500

    Theme toggles as new component

--- resources/views/partials/theme.blade.php
+++ resources/views/partials/theme.blade.php
@@ -2,7 +2,7 @@
 	Lazily copied from Laravel.com...
 	- Mode functions: https://github.com/laravel/laravel.com-next/blob/a2a98ffe47761f15903f7a1e80e2bf948771ea03/resources/js/components/theme.js
 	- Most of the inline script below: https://github.com/laravel/laravel.com-next/blob/a2a98ffe47761f15903f7a1e80e2bf948771ea03/resources/views/partials/layout.blade.php
-	- Modified to add an extra theme
+	- Modified to add an extra dark theme
 -->
 <script>
 	function toDarkMode() {
@@ -10,7 +10,7 @@
 		window.updateTheme()
 	}
 
-	function toFunMode() {
+	function toggleFunMode() {
 		localStorage.theme = 'fun'
 		window.updateTheme()
 	}
@@ -44,22 +44,27 @@
 
 		switch (localStorage.theme) {
 			case 'system':
+				document.documentElement.classList.remove('fun')
+
 				if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 					document.documentElement.classList.add('dark')
 				} else {
 					document.documentElement.classList.remove('dark')
 				}
+
 				document.documentElement.setAttribute('color-theme', 'system')
+
 				break
+
 			case 'dark':
 				document.documentElement.classList.add('dark')
 				document.documentElement.setAttribute('color-theme', 'dark')
 				break
+
 			case 'fun':
 				document.documentElement.classList.add('fun')
-				document.documentElement.classList.remove('dark')
-				document.documentElement.setAttribute('color-theme', 'fun')
 				break
+
 			case 'light':
 				document.documentElement.classList.remove('dark')
 				document.documentElement.classList.remove('fun')
