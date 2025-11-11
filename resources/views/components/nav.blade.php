ba7651b8c2af13efbcd3c5ea24a9a505165ce6cc
Elliot Derhay <elliot@elliotderhay.com>
2025-11-11 17:38:57 -0500

    Experiment using mobile menu for desktop

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -11,25 +11,30 @@
 		</a>
 	</div>
 
-	<div class="block lg:hidden mr-4">
-		<label for="menu-toggle"
-					 class="flex items-center text-bright-turquoise-900 dark:text-bright-turquoise-500 hover:text-bright-turquoise-600 dark:hover:text-white transition-colors duration-300">
-			<svg class="fill-current h-8 w-8" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-			</svg>
-		</label>
-	</div>
+	<x-nav.dropdown class='block lg:hidden mr-4'>
+		<x-slot:toggle>
+			<input type="checkbox" id="menu-toggle" name="menu-toggle" class="hidden absolute top-0 left-0 -z-50 peer">
 
-	<input type="checkbox" id="menu-toggle" name="menu-toggle" class="hidden absolute top-0 left-0 -z-50">
+			<label
+				for="menu-toggle"
+				class="flex items-center text-bright-turquoise-800 dark:text-bright-turquoise-500 hover:text-neutral-700 dark:hover:text-white transition-colors duration-300 peer-checked:text-neutral-700 dark:peer-checked:text-white">
+				<svg class="fill-current size-8" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+				</svg>
+			</label>
+		</x-slot:toggle>
 
-	<div
-		class="w-full block absolute lg:relative grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl mobile-menu">
-		<div class="text-md lg:grow">
+		{{ $slot }}
+	</x-nav.dropdown>
 
-			{{ $slot }}
+	{{--	<div--}}
+	{{--		class="w-full block absolute lg:relative grow lg:flex lg:items-center lg:w-auto text-center lg:text-right text-xl">--}}
+	{{--		<div class="text-md lg:grow">--}}
 
-		</div>
-	</div>
+	{{--			{{ $slot }}--}}
+
+	{{--		</div>--}}
+	{{--	</div>--}}
 
 	<x-nav.dropdown>
 		<x-slot:toggle>
