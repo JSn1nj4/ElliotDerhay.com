e1aad78d65304af83f92beffa331d202b63bf750
Elliot Derhay <elliot@elliotderhay.com>
2025-11-21 13:48:17 -0500

    Design track

--- resources/views/components/scroller/track.blade.php
+++ resources/views/components/scroller/track.blade.php
@@ -1,3 +1,9 @@
-<div class='absolute h-full top-0 left-0 text-white bg-blue-500'>
-	[!]
+<div class='absolute h-full flex top-0 left-0 text-white bg-neutral-800'>
+	<div class='relative w-5'>
+		<div class='w-full h-full bg-gradient-to-r from-slate-800/50 from-85% via-sky-300 to-neutral-800 to-90%'></div>
+	</div>
+	<div class='relative w-3'>
+		<div
+			class='w-full h-full bg-gradient-to-r from-slate-600 from-70% via-black  via-90% to-slate-500 to-90%'></div>
+	</div>
 </div>
