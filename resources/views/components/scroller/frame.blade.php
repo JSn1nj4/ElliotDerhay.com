09b64d3e454e6019ef8f5c540dc068e8e7ef838f
Elliot Derhay <elliot@elliotderhay.com>
2025-11-20 22:10:18 -0500

    Save body height to custom property

--- resources/views/components/scroller/frame.blade.php
+++ resources/views/components/scroller/frame.blade.php
@@ -1,3 +1,15 @@
-<div class='absolute -z-10 h-full w-full'>
+<div id='decorative_frame' class='absolute -z-10 h-full w-full'>
 	<!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
 </div>
+<script type='application/javascript'>
+	function getWindowHeight() {
+		document
+			.getElementById('decorative_frame')
+			.style
+			.setProperty('--body-height', `${document.documentElement.scrollHeight}px`)
+	}
+
+	window.addEventListener('resize', getWindowHeight)
+
+	getWindowHeight()
+</script>
