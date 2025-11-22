840abbe4a2f9d9043b310d3d509e08bbf521ec4a
Elliot Derhay <elliot@elliotderhay.com>
2025-11-21 21:14:49 -0500

    Make track more metallic

--- resources/views/components/scroller/track.blade.php
+++ resources/views/components/scroller/track.blade.php
@@ -1,9 +1,14 @@
-<div class='absolute h-full flex top-0 left-0 text-white bg-neutral-800'>
-	<div class='relative w-5'>
-		<div class='w-full h-full bg-gradient-to-r from-slate-800/50 from-85% via-sky-300 to-neutral-800 to-90%'></div>
-	</div>
-	<div class='relative w-3'>
+<div class='absolute h-full flex top-0 left-0 text-white bg-gradient-to-r from-slate-900 to-neutral-500 z-0'>
+	<div class=' relative w-5'>
 		<div
-			class='w-full h-full bg-gradient-to-r from-slate-600 from-70% via-black  via-90% to-slate-500 to-90%'></div>
+			class='w-full h-full bg-gradient-to-r from-slate-800/50 from-85% via-blue-300 to-neutral-800 to-90%'></div>
+	</div>
+	<div class='relative w-2'>
+		<div
+			class='w-full h-full bg-gradient-to-r from-slate-700 to-big-stone-950'></div>
+	</div>
+	<div class='relative w-0.5'>
+		<div
+			class='w-full h-full bg-gradient-to-r from-black from-50% via-slate-600  via-50% to-slate-500'></div>
 	</div>
 </div>
