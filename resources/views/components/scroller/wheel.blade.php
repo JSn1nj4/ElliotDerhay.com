48894fe2e2e0439c978534569982ecfe067a0fd3
Elliot Derhay <elliot@elliotderhay.com>
2025-11-22 17:50:10 -0500

    temp conic gradient on wheels
    Just as a placeholder

--- resources/views/components/scroller/wheel.blade.php
+++ resources/views/components/scroller/wheel.blade.php
@@ -1,10 +1,14 @@
 <div @class([
 	'fixed',
 	'size-20',
-	'bg-white',
+	'bg-conic-270' => !$mirror,
+	'bg-conic-90' => $mirror,
+	'from-big-stone-800',
+	'via-blue-200',
+	'to-big-stone-800',
 	'rounded-full',
 	'left-3' => !$mirror,
 	'right-3' => $mirror,
 	'-bottom-10',
 	'z-10',
-])></div>
+]) class='bg-conic-180 from-big-stone-800 via-blue-200 to-big-stone-800'></div>
