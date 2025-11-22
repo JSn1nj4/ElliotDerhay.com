83dc9a1d594fa96cea96ec9ae4578a94d91ad2ec
Elliot Derhay <elliot@elliotderhay.com>
2025-11-21 21:14:29 -0500

    Work on header a tad bit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,10 @@
 <header id="header"
-				class="flex-initial border-b-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500 bg-big-stone-200 dark:bg-black/50 transition-[border-width] fun:border-0">
-	<div class="container m-auto px-4">
+				class="flex-initial border-b-4 border-solid border-bright-turquoise-900 dark:border-bright-turquoise-500 bg-big-stone-200 dark:bg-big-stone-950 transition-[border-width] fun:border-slate-800 fun:drop-shadow-lg fun:drop-shadow-black/70 z-10">
+	<div class='w-full h-full bg-big-stone-200 dark:bg-black/50'>
+		<div class="container m-auto px-4">
 
-		{{ $slot }}
+			{{ $slot }}
 
+		</div>
 	</div>
 </header>
