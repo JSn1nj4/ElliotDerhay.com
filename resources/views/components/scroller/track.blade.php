9ff4b5140205ad0a63ec31c83a81defae1f96fd9
Elliot Derhay <elliot@elliotderhay.com>
2025-11-24 08:00:10 -0500

    Change track gradients

--- resources/views/components/scroller/track.blade.php
+++ resources/views/components/scroller/track.blade.php
@@ -6,13 +6,13 @@
 		</div>
 		<div class='relative w-0.5'>
 			<div
-				class='w-full h-full bg-gradient-to-r from-black from-50% via-slate-600  via-50% to-slate-500'></div>
+				class='w-full h-full bg-gradient-to-r from-transparent from-50% via-slate-600  via-50% to-slate-500'></div>
 		</div>
 		<div class='teeth relative w-0.5 h-full'></div>
 	</div>
 	<div
-		class='fixed {{ $mirror ? '-scale-x-100 right-5' : 'left-5' }} top-0 w-3 h-screen opacity-0 scroll:opacity-100 transition-opacity duration-700'>
+		class='fixed{{ $mirror ? ' -scale-x-100' : '' }} top-0 w-6 h-screen opacity-0 scroll:opacity-100 transition-opacity duration-700'>
 		<div
-			class='-ml-px2 w-3/5 h-full bg-gradient-to-t from-bright-turquoise-100 via-bright-turquoise-500/70 via-10% to-transparent to-50%'></div>
+			class='-ml-px2 w-full h-full bg-radial-[at_100%_100%] from-bright-turquoise-200/50 via-bright-turquoise-500/40 via-30% to-transparent to-50%'></div>
 	</div>
 </div>
