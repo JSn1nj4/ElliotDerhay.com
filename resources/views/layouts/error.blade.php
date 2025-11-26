acd941e9127b31bda3232ea863c047e779affd9e
Elliot Derhay <elliot@elliotderhay.com>
2025-11-26 18:13:29 -0500

    Start fixing remaining error page colors

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -15,22 +15,25 @@
 
 	@stack('head-extras')
 </head>
-<body class="bg-white dark:bg-black text-black dark:text-white font-sans flex flex-col status-page backlight">
-<div class="flex flex-col mx-auto container px-4 py-6 pt-16 sm:pt-32 md:pt-48 xl:pt-56 justify-center">
-	<div class="pb-4 w-full max-w-md mx-auto">
-		<h1 class="text-xl sm:text-4xl">
-			<span class="align-middle text-3xl sm:text-6xl font-extralight">{{ $errorCode }}</span>
-			<span
-				class="inline-block align-middle w-0 h-10 sm:h-16 border-solid border-r-2 border-bright-turquoise-500">&nbsp;</span>
-			<span class="align-middle leading-none">{{ $errorTitle }}</span>
-		</h1>
+<body class="bg-white dark:bg-big-stone-950 text-black dark:text-white font-sans status-page w-full h-full">
+<div class='bg-big-stone-200 dark:bg-black fun:bg-black/60 flex flex-col w-full h-full backlight'>
 
-		<div class="pt-4 text-md sm:text-lg leading-normal font-extralight">
-			@yield('status-body')
-		</div>
+	<div class="flex flex-col mx-auto container px-4 py-6 pt-16 sm:pt-32 md:pt-48 xl:pt-56 justify-center">
+		<div class="pb-4 w-full max-w-md mx-auto">
+			<h1 class="text-xl sm:text-4xl">
+				<span class="align-middle text-3xl sm:text-6xl font-extralight">{{ $errorCode }}</span>
+				<span
+					class="inline-block align-middle w-0 h-10 sm:h-16 border-solid border-r-2 border-bright-turquoise-500">&nbsp;</span>
+				<span class="align-middle leading-none">{{ $errorTitle }}</span>
+			</h1>
 
-		<div class="pt-4">
-			@yield('status-footer')
+			<div class="pt-4 text-md sm:text-lg leading-normal font-extralight">
+				@yield('status-body')
+			</div>
+
+			<div class="pt-4">
+				@yield('status-footer')
+			</div>
 		</div>
 	</div>
 </div>
