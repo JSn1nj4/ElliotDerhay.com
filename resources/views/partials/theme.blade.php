ff3b52aabb99d4c501a35a2386e174e88e7da0d7
Elliot Derhay <elliot@elliotderhay.com>
2025-11-30 16:18:02 -0500

    WIP: start wiring up display state machines
    Don't really need to do it this way, but it's a fun experiment.

--- resources/views/partials/theme.blade.php
+++ resources/views/partials/theme.blade.php
@@ -5,6 +5,168 @@
 	- Modified to add an extra dark theme
 -->
 <script>
+	const storageKeyId = 'storageKey'
+
+	class DisplayMode {
+		static resolve() {
+			const displayMode = localStorage.getItem(displayModes.storageKey)
+
+			if (displayMode === null || displayMode === undefined) {
+				return displayModes.system
+			}
+
+			if (!displayModes.hasOwnProperty(localStorage.getItem('displayMode'))) {
+				return displayModes.system
+			}
+
+			if (displayMode === storageKeyId) {
+				return displayModes.system
+			}
+
+			return displayModes[displayMode]
+		}
+
+		resolve() {
+			return this.constructor.resolve()
+		}
+
+		toSystemDefault() {
+			console.info('Display mode updated to system default.')
+			return displayModes.system
+		}
+
+		toLightMode() {
+			console.info('Display mode updated to light mode.')
+			return displayModes.light
+		}
+
+		toDarkMode() {
+			console.info('Display mode updated to dark mode.')
+			return displayModes.dark
+		}
+	}
+
+	class SystemDisplayMode extends DisplayMode {
+		resolve() {
+			return this
+		}
+
+		toSystemDefault() {
+			console.warn('Display mode is already system default. Doing nothing.')
+		}
+	}
+
+	class LightDisplayMode extends DisplayMode {
+		resolve() {
+			return this
+		}
+
+		toLightMode() {
+			console.warn('Display mode is already light mode. Doing nothing.')
+		}
+	}
+
+	class DarkDisplayMode extends DisplayMode {
+		resolve() {
+			return this
+		}
+
+		toDarkMode() {
+			console.warn('Display mode is already dark mode. Doing nothing.')
+		}
+	}
+
+	const displayModes = {
+		[storageKeyId]: 'displayMode',
+		'system': new SystemDisplayMode(),
+		'light': new LightDisplayMode(),
+		'dark': new DarkDisplayMode(),
+	}
+
+	class DisplayTheme {
+		static resolve() {
+			const displayTheme = localStorage.getItem(displayThemes.storageKey)
+
+			if (displayTheme === null || displayTheme === undefined) {
+				return displayThemes.light
+			}
+
+			if (!displayThemes.hasOwnProperty(displayTheme)) {
+				return displayThemes.light
+			}
+
+			if (displayTheme === storageKeyId) {
+				return displayThemes.light
+			}
+
+			return displayThemes[displayTheme]
+		}
+
+		resolve() {
+			return this.constructor.resolve()
+		}
+
+		toLightMetallicTheme() {
+			console.info('Display theme updated to default: light metallic.')
+			return displayThemes.light
+		}
+
+		toCyberneticTheme() {
+			console.info('Display theme updated to dark 1: cybernetic.')
+			return displayThemes.dark
+		}
+
+		toIndustrialTheme() {
+			console.info('Display theme updated to dark 2: industrial.')
+			return displayThemes.dark2
+		}
+	}
+
+	class LightMetallicTheme extends DisplayTheme {
+		resolve() {
+			return this
+		}
+
+		toLightMetallicTheme() {
+			console.warn('Display theme is already light metallic. Doing nothing.')
+		}
+	}
+
+	class CyberneticTheme extends DisplayTheme {
+		resolve() {
+			return this
+		}
+
+		toCyberneticTheme() {
+			console.warn('Display theme is already cybernetic. Doing nothing.')
+		}
+	}
+
+	class IndustrialTheme extends DisplayTheme {
+		resolve() {
+			return this
+		}
+
+		toIndustrialTheme() {
+			console.warn('Display theme is already industrial. Doing nothing.')
+		}
+	}
+
+	const displayThemes = {
+		[storageKeyId]: 'displayTheme',
+		'light': new LightMetallicTheme(),
+		'dark': new CyberneticTheme(),
+		'dark2': new IndustrialTheme(),
+	}
+
+	function initDisplayConfig() {
+		const displayMode = DisplayMode.resolve()
+	}
+
+	// ^^ new stuff ^^
+
+	// vv old stuff vv
+
 	function inTheDark() {
 		return document.documentElement.classList.contains('dark')
 	}
