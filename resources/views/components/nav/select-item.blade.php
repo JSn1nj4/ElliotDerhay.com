a78383f35543cda831e45d24035c54a22c104d68
Elliot Derhay <elliot@elliotderhay.com>
2025-12-06 13:49:32 -0500

    Update dropdown appearance

--- resources/views/components/nav/select-item.blade.php
+++ resources/views/components/nav/select-item.blade.php
@@ -2,21 +2,23 @@
 	'label'
 ])
 <div
-	class='justify-items-start place-items-center gap-2 text-left text-bright-turquoise-800 dark:text-bright-turquoise-500 hover:text-neutral-700 dark:hover:text-white transition-colors duration-300'
+	class='w-full relative justify-items-start place-items-center gap-2 text-left text-bright-turquoise-800 dark:text-bright-turquoise-500 hover:text-neutral-700 dark:hover:text-white transition-colors duration-300 uppercase'
 	x-data='<?= $dataLabel() ?>'
 >
 	@isset($label)
-		<label class='' for='{{ $id }}'>{{ $label }}</label>
+		<label class='text-sm' for='{{ $id }}'>{{ $label }}</label>
 	@endisset
 	<select
-		class=''
+		class='w-full rounded border border-slate-500 dark:border-slate-700 dark2:border-slate-600 bg-slate-200 dark:bg-neutral-950 dark2:bg-slate-700 px-4 py-1 appearance-none transition-colors'
 		name='{{ $name }}'
 		id='{{ $id }}'
 		x-model='value'
 		x-on:change='send'
+		x-ref='list'
 	>
 		{{ $slot }}
 	</select>
+	<x-fas-caret-down class="absolute top-3 right-4 h-full size-3 pointer-events-none" />
 </div>
 <script>
 	document.addEventListener('alpine:init', () => {
