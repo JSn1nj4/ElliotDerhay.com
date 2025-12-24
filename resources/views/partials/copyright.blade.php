ca929fb7968a776730d6d482bd893260704efc16
Elliot Derhay <elliot@elliotderhay.com>
2025-12-24 00:29:54 -0500

    Add missing wire:navigate to a few links
    Missing this on a few attributes was causing horrendous flash (the brand link, and footer links for Privacy Policy and Credits).

--- resources/views/partials/copyright.blade.php
+++ resources/views/partials/copyright.blade.php
@@ -1,2 +1,3 @@
 <p class="text-black dark:text-slate-500">Copyright &copy; {{ today()->year }} Elliot Derhay | <a
-		href="{{ route('privacy', absolute: false) }}">Privacy Policy</a></p>
+		href="{{ route('privacy', absolute: false) }}" wire:navigate>Privacy Policy</a> | <a
+		href='{{ route('credits', absolute: false) }}' wire:navigate>Credits</a></p>
