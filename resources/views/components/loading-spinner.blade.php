eedc2b09ea0e14b919326b9ebfc09f98f5f407fe
Elliot Derhay <elliot@elliotderhay.com>
2026-01-31 10:48:57 -0500

    Convert old atom spinner to blade component

---
+++ resources/views/components/loading-spinner.blade.php
@@ -0,0 +1,21 @@
+<div>
+	<div class="atom-spinner w-[60px] h-[60px] overflow-hidden">
+		<div class="spinner-inner relative block w-full h-full">
+			<div
+				class="spinner-line absolute w-full h-full rounded-[50%] border-l-solid border-t-solid border-t-transparent border-l-[calc(60px/25)] border-t-[calc(60px/25)]  border-l-bright-turquoise-600 dark:border-l-bright-turquoise-500"
+			></div>
+			<div
+				class="spinner-line absolute w-full h-full rounded-[50%] border-l-solid border-t-solid border-t-transparent border-l-[calc(60px/25)] border-t-[calc(60px/25)]  border-l-bright-turquoise-600 dark:border-l-bright-turquoise-500"
+			></div>
+			<div
+				class="spinner-line absolute w-full h-full rounded-[50%] border-l-solid border-t-solid border-t-transparent border-l-[calc(60px/25)] border-t-[calc(60px/25)]  border-l-bright-turquoise-600 dark:border-l-bright-turquoise-500"
+			></div>
+			<!--Chrome renders little circles malformed :(-->
+			<div
+				class="spinner-circle block absolute top-[50%] left-[50%] -translate-[50%] text-bright-turquoise-700 dark:text-bright-turquoise-400 text-[calc(60px*0.24)]"
+			>
+				&#9679;
+			</div>
+		</div>
+	</div>
+</div>
