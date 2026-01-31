2fdab17e496c1dab2f94c54e59d5769e04139c3a
Elliot Derhay <elliot@elliotderhay.com>
2026-01-31 16:44:40 -0500

    Set up new migration

---
+++ database/migrations/2026_01_31_210921_replace_post_publishing_fields.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::whenTableDoesntHaveColumn('posts', 'status', function (Blueprint $table) {
+			$table->tinyText('status')->default('draft');
+		});
+
+		Schema::whenTableDoesntHaveColumn('posts', 'scheduled_for', function (Blueprint $table) {
+			$table->timestamp('scheduled_for')->nullable();
+		});
+
+		DB::transaction(function () {
+			DB::table('posts')
+				->select('*')
+				->where('published', true)
+				->update(['status' => 'published']);
+		});
+
+		Schema::whenTableHasColumn('posts', 'published', function (Blueprint $table) {
+			$table->removeColumn('published');
+		});
+	}
+};
