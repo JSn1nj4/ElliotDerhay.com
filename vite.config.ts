609a96bdf5fdf8f8cccadc6bbc6a66b503a429b6
Elliot Derhay <elliot@elliotderhay.com>
2026-01-31 10:49:14 -0500

    Remove leftover Vue stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig, loadEnv} from 'vite'
 import laravel, {refreshPaths} from 'laravel-vite-plugin'
-import vuePlugin from '@vitejs/plugin-vue'
 import * as fs from 'fs'
 import {SecureServerOptions} from 'node:http2'
 import tailwindcss from '@tailwindcss/vite'
@@ -26,7 +25,6 @@ export default defineConfig(({mode}) => {
 					},
 				],
 			}),
-			vuePlugin(),
 			tailwindcss(),
 		],
 
