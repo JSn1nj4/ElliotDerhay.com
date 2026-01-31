27366643c084193387e6c60ffaee1ba4108287f3
JSn1nj4 <elliot@elliotderhay.com>
2026-01-31 13:52:20 -0500

    JSN-326: Prevent flash when transitioning between livewire pages (#291)
    * Add class that prevents flashing
    
    * Switch to .navigating class and variant
    
    * Small proof this works
    
    * Fix transitions
    
    * Update changelog

--- resources/views/components/scroller/track.blade.php
+++ resources/views/components/scroller/track.blade.php
@@ -1,6 +1,6 @@
 <div class='sidebar-track relative h-full w-6 overflow-x-clip'>
 	<div
-		class='relative h-full flex{{ $mirror ? ' -scale-x-100' : '' }} {{ $mirror ? 'translate-x-32' : '-translate-x-32' }} dark2:sm:translate-x-0 transition-transform'>
+		class='relative h-full flex{{ $mirror ? ' -scale-x-100' : '' }} {{ $mirror ? 'translate-x-32' : '-translate-x-32' }} dark2:sm:translate-x-0 transition-transform navigating:transition-none'>
 		<div class='relative w-5'>
 			<div
 				class='w-full h-full bg-gradient-to-r from-slate-800 via-slate-700 via-80% to-slate-600 border-r-2 border-solid border-r-slate-800'></div>
@@ -12,7 +12,7 @@
 		<div class='teeth relative w-0.5 h-full'></div>
 	</div>
 	<div
-		class='fixed top-0{{ $mirror ? ' -scale-x-100' : '' }} {{ $mirror ? 'translate-x-32' : '-translate-x-32' }} dark2:sm:translate-x-0 w-6 h-screen opacity-0 scroll:opacity-100 transition-all duration-700 ease-in-out'>
+		class='fixed top-0{{ $mirror ? ' -scale-x-100' : '' }} {{ $mirror ? 'translate-x-32' : '-translate-x-32' }} dark2:sm:translate-x-0 w-6 h-screen opacity-0 scroll:opacity-100 transition-all navigating:transition-none duration-700 ease-in-out'>
 		<div
 			class='-ml-px2 w-full h-full bg-radial-[at_100%_100%] from-bright-turquoise-200/50 via-bright-turquoise-500/40 via-30% to-transparent to-50%'></div>
 	</div>
