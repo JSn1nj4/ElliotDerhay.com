371c8d4dc277a754ddfb70ad34952a243d032f94
Elliot Derhay <elliot@elliotderhay.com>
2026-02-23 22:10:43 -0500

    Change TelegramRecipient signature

--- app/DataTransferObjects/TelegramRecipient.php
+++ app/DataTransferObjects/TelegramRecipient.php
@@ -5,11 +5,11 @@ namespace App\DataTransferObjects;
 readonly class TelegramRecipient
 {
 	public function __construct(
-		public string $recipient,
+		public string $telegram_chat_id,
 	) {}
 
 	public function __toString(): string
 	{
-		return $this->recipient;
+		return $this->telegram_chat_id;
 	}
 }
