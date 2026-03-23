ee84494930abef12127f227b76aa52df629a5716
Elliot Derhay <elliot@elliotderhay.com>
2026-03-23 08:30:09 -0400

    Make preview height 100% of container

--- resources/views/filament/forms/components/content-preview.blade.php
+++ resources/views/filament/forms/components/content-preview.blade.php
@@ -1,13 +1,15 @@
 <x-dynamic-component
 	:component="$getFieldWrapperView()"
 	:field="$field"
+	class='h-full grid-rows-[auto_1fr]'
 >
 	@php $preparePreview($get($field->getSourceField())) @endphp
 	<div
 		x-data="{ state: $wire.$entangle(@js($getStatePath())) }"
+		class='h-full'
 		{{ $getExtraAttributeBag() }}
 	>
-		<iframe src="{{ $getRoute() }}" class='w-full h-128'></iframe>
+		<iframe src="{{ $getRoute() }}" class='w-full min-h-128 h-full'></iframe>
 	</div>
 	@php $purgeStalePreview() @endphp
 </x-dynamic-component>
